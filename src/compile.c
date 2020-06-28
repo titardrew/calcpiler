7cf42c64ed88dbf8bfe2f5bc108dc395812beba6
Tytarenko <titarenkoan@gmail.com>
2020-06-28 23:29:54 +0300

    Lexer added.

--- src/compile.c
+++ src/compile.c
@@ -1,5 +1,105 @@
 #include "stdio.h"
 #include "stdlib.h"
+#include "stdarg.h"
+#include "ctype.h"
+
+char *global_code;
+
+typedef enum Status {
+    FAILURE = 0,
+    SUCCESS = 1
+} Status;
+
+void panic_at(char *location, char *reason, ...) {
+    va_list ap;
+    va_start(ap, reason);
+
+    int position = location - global_code;
+    fprintf(stderr, "%s\n", global_code);
+    fprintf(stderr, "%*s", position, "");
+    fprintf(stderr, "^ ");
+    vfprintf(stderr, reason, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
+typedef enum TokenKind {
+    TK_RESERVED = 0,
+    TK_NUM = 1,
+    TK_EOF = 2
+} TokenKind;
+
+const char *global_tk_repr[] = {
+    "Reserved", "Number", "End Of File"};
+
+typedef struct Token {
+    TokenKind kind;
+    long int num_val;
+    char *str;
+    struct Token *next;
+} Token;
+
+Token * create_token(TokenKind kind, Token *current_token, char *str) {
+    Token *new_token = (Token *)calloc(1, sizeof(Token));
+    new_token->kind = kind;
+    new_token->str = str;
+    current_token->next = new_token;
+    return new_token;  // now it is current
+}
+
+Token * tokenize(char *p) {
+    Token head;
+    head.next = NULL;
+    Token *current_token = &head;
+
+    while (*p) {
+        if (isspace(*p)) {
+            ++p;
+            continue;
+        }
+
+        if (*p == '-' || *p == '+') {
+            current_token = create_token(TK_RESERVED, current_token, p);
+            ++p;
+            continue;
+        }
+
+        if (isdigit(*p)) {
+            current_token = create_token(TK_NUM, current_token, p);
+            current_token->num_val = strtol(p, &p, 10);
+            continue;
+        }
+
+        panic_at(p, "Lexing error. Can't figure out token. [TODO: verbose]");
+    }
+
+    create_token(TK_EOF, current_token, NULL);
+    return head.next;
+}
+
+Token *global_token_ptr;
+
+Status try_eat_op_token(char op) {
+    if (global_token_ptr->kind != TK_RESERVED ||
+          *global_token_ptr->str != op) {
+        return FAILURE;
+    }
+    global_token_ptr = global_token_ptr->next;
+    return SUCCESS;
+}
+
+Token * eat_number_token() {
+    if (global_token_ptr->kind != TK_NUM) {
+        panic_at(
+            global_token_ptr->str,
+            "Parsing error: Wrong Token (Got %s, expected %s)",
+            global_tk_repr[global_token_ptr->kind], global_tk_repr[TK_NUM]
+        );
+    }
+    Token *num_token = global_token_ptr;
+    global_token_ptr = global_token_ptr->next;
+    return num_token;
+}
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
@@ -7,33 +107,29 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    char *p = argv[1];
+    global_code = argv[1];
+    global_token_ptr = tokenize(global_code);
 
-    printf(".intel_syntax noprefix\n");
+    printf(".intel_syntax_noprefix\n");
     printf(".globl _main\n");
     printf("_main:\n");
 
+    printf(" mov rax, %ld\n", eat_number_token()->num_val);
 
-    printf("  mov rax, %ld\n", strtol(p, &p, 10));
-
-    while (*p) {
-        if (*p == '+') {
-            ++p;
-            printf("  add rax, %ld\n", strtol(p, &p, 10));
-            continue;
-        } 
-
-        if (*p == '-') {
-            ++p;
-            printf("  sub rax, %ld\n", strtol(p, &p, 10));
+    while (global_token_ptr->kind != TK_EOF) {
+        if (try_eat_op_token('+')) {
+            printf(" add rax, %ld\n", eat_number_token()->num_val);
             continue;
         }
 
-        fprintf(stderr, "Unknown token: '%c'\n", *p);
-        return 1;
+        if (try_eat_op_token('-')) {
+            printf(" sub rax, %ld\n", eat_number_token()->num_val);
+            continue;
+        }
+
+        panic_at(global_token_ptr->str, "Parsing error: Wrong token");
     }
 
-    printf("  ret");
-
+    printf(" ret \n");
     return 0;
 }
--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 test_retval() {
-    input=$1
-    expected_output=$2
+    input="$1"
+    expected_output="$2"
 
-    build/compile $1 > build/temp_asm.s
+    build/compile "${input}" > build/temp_asm.s
     cc -o build/test_retval build/temp_asm.s
     build/test_retval
     output=$?
@@ -17,8 +17,24 @@ test_retval() {
     fi
 }
 
+test_comp() {
+    input="$1"
+    expected_output="$2"
+
+    build/compile "${input}" > build/temp_asm.s
+    output=$?
+
+    if [ "${output}" = "${expected_output}" ]; then
+        echo "Exitcode: ${output}"
+    else
+        echo "Test failed. Expected: ${expected_output}, Got: ${output}"
+        exit 1
+    fi
+}
+
 test_retval 0 0
 test_retval "99+1-5+10-3" 102
-test_retval "99+1" 100
+test_retval " 99 + 1 - 9 +    9 " 100
+test_comp "99 ++ 1" 1
 
 echo "Success!"
